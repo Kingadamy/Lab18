c1a7d4aeb321a7e31a0ab42cd73ca151ea81968d
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-04 20:59:59 -0700

    update

--- lab18.cpp
+++ lab18.cpp
@@ -1,3 +1,65 @@
 #include <iostream>
 
 using namespace std;
+
+const Node{   // create a struct for the node
+    string movieRating;
+    node* next;
+}
+
+struct Node *head = nullptr; // set head to null
+
+void insert(int n){  // function to insert a new node at the head
+    struct Node *newNode = new Node;
+    newNode -> data = n;
+    newNode -> next = head;
+    head = newNode;
+}
+
+
+int main(){
+
+    int choice; // create int to choose to add to head or tail
+    
+    cout << "Which linked list method should we use?" << endl;
+    cout << "\t[1] New nodes are added at the head of the linked list" << endl;
+    cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
+
+    cout << "\tChoice: ";
+    cin >> choice;
+
+    if (choice == 1){
+        // add nodes at head
+    } else if (choice == 2){
+        // add nodes at tail
+    } else {
+        cout << "Invalid choice. Try again" << endl;
+        return 1;
+    }
+
+    cout << "Enter review rating 0-5: ";
+    
+    
+    double rating;
+
+
+
+}
+
+
+
+
+// Which linked list method should we use?
+//     [1] New nodes are added at the head of the linked list
+//     [2] New nodes are added at the tail of the linked list
+//     Choice: 2
+// Enter review rating 0-5: 4.8
+// Enter review comments: Oscar contender
+// Enter another review? Y/N: y
+// Enter review rating 0-5: 4.1
+// Enter review comments: Brilliant lead acting
+// Enter another review? Y/N: n
+// Outputting all reviews:
+//     > Review #1: 4.8: Oscar contender
+//     > Review #2: 4.1: Brilliant lead acting
+//     > Average: 2.96667
