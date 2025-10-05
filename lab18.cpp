1a422136fa4fe75edf6881891fdac0d2a48bb6e3
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-04 21:39:16 -0700

    update

--- lab18.cpp
+++ lab18.cpp
@@ -2,37 +2,33 @@
 
 using namespace std;
 
-const Node{   // create a struct for the node
+struct Node{   // create a struct for the node
    double rating;
    string comments;
-    node* next;
-}
+   Node* next;
+};
 
 struct Node *head = nullptr; // set head to null
 
+void addAtHead(double rating, string comments); // function prototype to add at head
+void addAtTail(double rating, string comments); // function prototype to add at tail
+void display(); // function prototype to display the list
 
 int main(){
 
     int choice; // create int to choose to add to head or tail
     
+       
+    char another;
+    double rating;
+    string comments;
+
     cout << "Which linked list method should we use?" << endl;
     cout << "\t[1] New nodes are added at the head of the linked list" << endl;
     cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
 
     cout << "\tChoice: ";
     cin >> choice;
-
-    if (choice == 1){
-        // add nodes at head
-    } else if (choice == 2){
-        // add nodes at tail
-    } else {
-        cout << "Invalid choice. Try again" << endl;
-        return 1;
-    }
-
-   
-      char another;
 
     cout << "Enter review rating 0-5: " << endl;
     cin >> rating;
@@ -59,6 +55,17 @@ int main(){
         cin.ignore();
     }
 
+        if (choice == 1){
+        addAtHead(rating, comments); // call function to add at head
+    } else if (choice == 2){
+        addAtTail(rating, comments);
+        // add nodes at tail
+    } else {
+        cout << "Invalid choice. Try again" << endl;
+        return 1;
+    }
+
+
     cout << "Outputting all reviews: " << endl;
     // output all reviews here
     // make sure to output review number, rating, and comments in same format as example
@@ -82,7 +89,6 @@ void addAtTail(double rating, string comments){
     newNode -> rating = rating; // input data
     newNode -> comments = comments; //input data
     newNode -> next = nullptr; // point new node to tail
-    nullptr = newNode; // move head to point to new node
 
     if(head == nullptr) { //check if list is empty
         head = newNode; // if it is then we set head to new node
@@ -95,3 +101,12 @@ void addAtTail(double rating, string comments){
         temp -> next = newNode; // point last node to new node
     }
 }
+
+void display(){ //display outputs now
+    struct Node *temp = head; // transverse the list
+    while(temp != nullptr){}
+        cout << temp -> rating << endl; // output rating
+        cout << temp -> comments << endl; // output comments
+        temp = temp -> next; // move to next node
+}
+
