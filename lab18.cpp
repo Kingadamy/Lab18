00af6fbc934e3b47cd66538cb3fd7c3a6a2a44e3
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-04 22:20:25 -0700

    update

--- lab18.cpp
+++ lab18.cpp
@@ -17,7 +17,6 @@ void display(); // function prototype to display the list
 int main(){
 
     int choice; // create int to choose to add to head or tail
-    
        
     char another;
     double rating;
@@ -26,21 +25,30 @@ int main(){
     cout << "Which linked list method should we use?" << endl;
     cout << "\t[1] New nodes are added at the head of the linked list" << endl;
     cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
-
     cout << "\tChoice: ";
     cin >> choice;
 
-    cout << "Enter review rating 0-5: " << endl;
+    cout << "Enter review rating 0-5: " << endl;  // get first review
     cin >> rating;
     cin.ignore();
 
     cout << "Enter review comments: \n";
     getline(cin, comments);
- 
 
+     if (choice == 1){
+        addAtHead(rating, comments); // call function to add at head
+    } else if (choice == 2){
+        addAtTail(rating, comments);
+        // add nodes at tail
+    } else {
+        cout << "Invalid choice. Try again" << endl;
+        return 1;
+    }
+ 
     cout << "Enter another review? Y/N: " << endl;
     cin >> another;
     cin.ignore();
+
 
     while (another == 'y' || another == 'Y'){  // while loop to continue adding reviews until user says no
         cout << "Enter review rating 0-5: " << endl;
@@ -50,19 +58,15 @@ int main(){
         cout << "Enter review comments: \n";
         getline(cin, comments);
 
+         if (choice == 1) {
+            addAtHead(rating, comments);
+        } else if (choice == 2) {
+            addAtTail(rating, comments);
+        }
+
         cout << "Enter another review? Y/N: " << endl;
         cin >> another;
         cin.ignore();
-    }
-
-        if (choice == 1){
-        addAtHead(rating, comments); // call function to add at head
-    } else if (choice == 2){
-        addAtTail(rating, comments);
-        // add nodes at tail
-    } else {
-        cout << "Invalid choice. Try again" << endl;
-        return 1;
     }
 
 
@@ -71,6 +75,9 @@ int main(){
     // make sure to output review number, rating, and comments in same format as example
     // calculate and output average rating
 
+    display(); // call display function 
+
+    return 0;
 
 }
 
@@ -103,6 +110,11 @@ void addAtTail(double rating, string comments){
 }
 
 void display(){ //display outputs now
+
+    int count = 1; // review #
+    double sum = 0; // total rating
+    int total = 0; // number of reviews
+
     struct Node *temp = head; // transverse the list
     while(temp != nullptr){
         cout << temp -> rating << endl; // output rating
